cc1633d42c6cd1ce8cb78b1b2a85f8ffb07e4519
felipevillajr <felipevilla2010@live.com>
2022-04-07 20:01:15 -0400

    removed unused import

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
-import logo from "../../Assets/images/felipeweblogo2single.png";
+// import logo from "../../Assets/images/felipeweblogo2single.png";
 
 export default function header() {
   return (
