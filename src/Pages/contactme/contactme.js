8bca50c3669937e9ef8bfb999a20131eb2116a8f
felipevillajr <felipevilla2010@live.com>
2022-06-08 16:14:31 -0400

    added github link in aboutme page

--- src/Pages/contactme/contactme.js
+++ src/Pages/contactme/contactme.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function contactme() {
   return (
     <div className="aboutme">
+      <br/>
       <h1>Felipe Villa</h1>
+      <br/>
       <h3>
         I am new to the tech scene and looking for oppertunities. I enjoy Web
         design and learning new technologies. Resently I have been learning
@@ -11,6 +13,10 @@ export default function contactme() {
         the use of React.js with a focus on Node as the basis of my
         webdevelopment.
       </h3>
+      <br/>
+      <h3> Please visit my Github for more on code I have worked on</h3>
+      <br/>
+      <a href="https://github.com/Felipevillajr"><h3>GitHub Link!</h3></a>
     </div>
   );
 }
